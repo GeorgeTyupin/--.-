f476e32ff33ccefb01d12ba744fcfa06931ede5e
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2018-11-24 13:32:55 +0300

    Add files via upload

---
+++ ветвь 4/Шаги.js
@@ -0,0 +1,13 @@
+	function change_one(){
+		document.getElementById("change").innerHTML = "New Text 1";
+	}
+		function change_two(){
+		document.getElementById("change").innerHTML = "New Text 2";
+	}
+		function change_three(){
+		document.getElementById("change").innerHTML = "New Text 3";
+	}
+		function change_four(){
+		document.getElementById("change").innerHTML = "New Text 4";
+	}
+		

ee2b3f9d20aaa296515ae56ba2c0f7bb0ffd7981
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2018-11-28 21:24:16 +0300

    Add files via upload

--- ветвь 4/Шаги.js
+++ ветвь 4/Шаги.js
@@ -1,13 +1,16 @@
-	function change_one(){
-		document.getElementById("change").innerHTML = "New Text 1";
+	function change_one()
+                          {
+
+      document.getElementById("img1").src="картинки/шаги 1 сбор.jpeg" ;
+ document.getElementById("change").innerHTML = "awyefh "  ;
 	}
 		function change_two(){
+        document.getElementById("img1").src="картинки/3.png" ;    
 		document.getElementById("change").innerHTML = "New Text 2";
 	}
 		function change_three(){
-		document.getElementById("change").innerHTML = "New Text 3";
+  document.getElementById("change").innerHTML = "New Text 3";
 	}
 		function change_four(){
 		document.getElementById("change").innerHTML = "New Text 4";
 	}
-		

5ad0baacea35ae276c552aa242f42ad0317a5555
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2018-12-08 19:18:17 +0300

    Add files via upload

--- ветвь 5/Шаги.js
+++ ветвь 5/Шаги.js
@@ -1,20 +1,20 @@
 	function change_one()
                           {
                     
-      document.getElementById("img1").src="картинки/шаг2.jpg" ;
+      document.getElementById("img1").src="2.jpg" ;
  document.getElementById("change").innerHTML = "Группировка первичной информации "  ;
 	}
 		function change_two(id){
        /* document.getElementsByClassName("active").classList.remove("active");
         document.getElementsByClassName(id).[0].classList.add("active");  */
-        document.getElementById("img1").src="картинки/опрос.jpg" ;    
+        document.getElementById("img1").src="0.jpg" ;    
 		document.getElementById("change").innerHTML = "Опрос родственников";
 	}
 		function change_three(){
-  document.getElementById("img1").src="картинки/шаги 1 сбор.jpeg" ;    
+  document.getElementById("img1").src="h1.jpeg" ;    
 		document.getElementById("change").innerHTML = "Архивы, музеи, выставки";
 	}
 		function change_four(){
-		document.getElementById("img1").src="картинки/Безымянный.png " ;    
+		document.getElementById("img1").src="b.png " ;    
 		document.getElementById("change").innerHTML = "Интернет, WWW сайты";
 	}
